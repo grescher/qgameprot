ca80bee6db695fe02ee808ae6b554b3b164387c9
Serge Chernov <gres.cher@icloud.com>
2020-05-31 21:37:16 +0300

    player.go -> add func toCoords()

--- player.go
+++ player.go
@@ -8,10 +8,13 @@ import (
 
 /* Types */
 
+// Coords type contains coorginates on the game map.
+type Coords [2]int
+
 // Player type.
 type Player struct {
-	Location int  // Tracks in which sector the player currently is.
-	IsBot    bool // True if a player is bot.
+	Location Coords // Tracks in which sector the player currently is.
+	IsBot    bool   // True if a player is bot.
 }
 
 /* Variables for the local use */
@@ -45,7 +48,7 @@ func (p *Player) InitPos() {
 			log.Fatal(err)
 		}
 		fmt.Printf("You chose sector %d\n", choice)
-		initHumPos, p.Location = choice, choice
+		initHumPos, p.Location = choice, toCoords(choice)
 	} else {
 		// The bot player initial sector.
 		choice = rand.Intn(MapMax) + 1
@@ -53,7 +56,7 @@ func (p *Player) InitPos() {
 			// Reassign location if it's equal to the human player location.
 			choice = rand.Intn(MapMax) + 1
 		}
-		p.Location = choice
+		p.Location = toCoords(choice)
 		fmt.Printf("The opponent's clan chose sector %d\n", choice)
 	}
 }
@@ -84,4 +87,11 @@ func readUserInput(lim int) (input int, err error) {
 		}
 	}
 	return input, nil
+}
+
+// Converts map sector number into map coordinates.
+func toCoords(n int) (xy Coords) {
+	xy[0] = (n - 1) / MapWidth
+	xy[1] = (n - 1) % MapWidth
+	return xy
 }
